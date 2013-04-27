93814da9c51c5b073e44a4cb81b47c581a93bf27
Inge E. Halsaunet <halsaune@stud.ntnu.no>
2013-04-27 17:58:06 +0200

    Added draw function and objects

--- theGameOfAllGames.js
+++ theGameOfAllGames.js
@@ -1,13 +1,31 @@
+var ball = {
+	"r" : 5,
+	"x" : 400,
+	"y" : 600,
+	"dx" : 10,
+	"dy" : 10
+}
+
+var board = {
+	"width" : 800,
+	"height" : 640
+}
+
+var context;
+
 function testRun() {
-	var c=document.getElementById("gameBoard");
-	var ctx=c.getContext("2d");
-	ctx.fillStyle="#444444";
-	ctx.fillRect(0,0,800,640);
-	 
-	ball= gameBoard.getContext('2d');
-	ball.beginPath();
-	ball.fillStyle="#ff0000";
-	ball.arc(400,600,5,0,Math.PI*2,true); 
-	ball.closePath();
-	ball.fill();
+	var c = document.getElementById("gameBoard");
+	context = c.getContext("2d");
+	context.fillStyle = "#444444";
+	context.fillRect(0, 0, board.width, board.height);
+
+	draw();
+}
+
+function draw() {
+	context.beginPath();
+	context.fillStyle = "#ff0000";
+	context.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2, true);
+	context.closePath();
+	context.fill();
 }
