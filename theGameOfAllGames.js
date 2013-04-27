e3df62243b8026d4fe3a052f6ed6d670f24b9b3a
jorhol <holmefjord@gmail.com>
2013-04-27 17:46:37 +0200

    added ball

--- theGameOfAllGames.js
+++ theGameOfAllGames.js
@@ -1,6 +1,13 @@
 function testRun() {
 	var c=document.getElementById("gameBoard");
-	 var ctx=c.getContext("2d");
-	 ctx.fillStyle="#444444";
-	 ctx.fillRect(0,0,800,640);
+	var ctx=c.getContext("2d");
+	ctx.fillStyle="#444444";
+	ctx.fillRect(0,0,800,640);
+	 
+	ball= gameBoard.getContext('2d');
+	ball.beginPath();
+	ball.fillStyle="#ff0000";
+	ball.arc(400,600,5,0,Math.PI*2,true); 
+	ball.closePath();
+	ball.fill();
 }
