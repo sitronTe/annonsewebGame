502e48cc2de8a111f60feb9ca61389ee4bd3b6a4
Inge E. Halsaunet <halsaune@stud.ntnu.no>
2013-04-27 18:19:22 +0200

    set bg color, added move

--- theGameOfAllGames.js
+++ theGameOfAllGames.js
@@ -2,8 +2,8 @@ var ball = {
 	"r" : 5,
 	"x" : 400,
 	"y" : 600,
-	"dx" : 10,
-	"dy" : 10
+	"dx" : 1,
+	"dy" : 1
 }
 
 var board = {
@@ -16,13 +16,22 @@ var context;
 function testRun() {
 	var c = document.getElementById("gameBoard");
 	context = c.getContext("2d");
-	context.fillStyle = "#444444";
-	context.fillRect(0, 0, board.width, board.height);
 
+	setInterval(tick, 10);
+}
+
+function tick() {
+	move();
 	draw();
 }
 
+function move() {
+	ball.x += ball.dx;
+	ball.y -= ball.dy;
+}
+
 function draw() {
+	context.clearRect(0, 0, board.width, board.height);
 	context.beginPath();
 	context.fillStyle = "#ff0000";
 	context.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2, true);
